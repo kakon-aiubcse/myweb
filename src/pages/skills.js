b3568c42d18c2765ea2c9ff59595c48dd07116f2
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-06-23 04:48:30 +0600

    completed for rest 2 part of skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -14,7 +14,7 @@ const Skills = () => {
           <Header />
         </div>
       )}
-      <div className="flex flex-col max-w-[1440px] pt-[50px] bg-slate-50 relative xs:w-screen xs:h-[2000px] xs:flex xs:bg-slate-300 xs:top-[0px] xs:items-center">
+      <div className="flex flex-col max-w-[1440px] pt-[50px] bg-slate-50 relative xs:w-screen xs:h-[4000px] xs:flex xs:bg-slate-300 xs:top-[0px] xs:items-center">
         <div className="flex relative w-[200px] xs:relative xs:right-[550px]">
           <Link
             href="/skills"
@@ -180,22 +180,40 @@ const Skills = () => {
           </div>
           <div
             className="flex w-[1130px] h-[100px] relative bg-slate-100  text-gray-700 rounded-[10px] hover:border-2
-           hover:transition-all hover:duration-1000 hover:border-sky-700 hover:shadow-lg hover:pb-2"
+           hover:transition-all hover:duration-1000 hover:border-sky-700 hover:shadow-lg hover:pb-2
+           xs:flex xs:flex-col xs:w-[300px] xs:relative xs:items-center xs:h-[350px] xs:left-[290px] xs:bg-slate-950 xs:text-sky-300"
           >
-            <h1 className="flex relative left-[40px] hover:text-sky-700 ">
-              <span className="text-[20px] leading-[20px] font-bold font-os flex relative left-[200px] top-[20px] hover:font-extrabold">
+            <h1
+              className="flex relative left-[40px] hover:text-sky-700 
+            xs:flex xs:flex-col xs:items-start xs:left-0 xs:relative xs:hover:text-sky-500 xs:top-[225px]"
+            >
+              <span
+                className="text-[20px] leading-[20px] font-bold font-os flex relative left-[200px] top-[20px] hover:font-extrabold
+              xs:relative xs:left-[30px] xs:flex xs:h-[50px] xs:w-[200px] xs:text-[25px] xs:font-mono xs:top-[-200px]"
+              >
                 Technologies
               </span>
-              <span className="text-[15px] font-roboto leading-[15px] font-[400] flex relative top-[60px] left-[40px] hover:font-medium">
-                Prisma, Stripe, React, Nodejs
+              <span className="xs:hidden text-[15px] font-roboto leading-[15px] font-[400] flex relative top-[60px] left-[40px] hover:font-medium">
+                Prisma, Stripe, Nodejs
+              </span>
+              <span
+                className="hidden text-[15px] font-roboto leading-[15px] h-[100px] w-[250px] font-[400]  relative top-[60px] left-[-45px] hover:font-[500]
+              xs:text-[20px] xs:flex xs:flex-col xs:leading-[50px] xs:relative xs:w-[80px] xs:h-[400px] xs:right-10 xs:top-[-200px] xs:ml-4 xs:text-center
+              "
+              >
+                Prisma Stripe Nodejs
               </span>
             </h1>
-            <ul className="flex relative left-[385px]  space-x-2  hover:transition-all hover:duration-1000">
+            <ul
+              className="flex relative left-[385px]  space-x-2  hover:transition-all hover:duration-1000
+            xs:relative xs:flex xs:flex-col xs:top-[-380px] xs:left-[60px] xs:items-center xs:justify-center gap-1"
+            >
               <li>
                 <img
                   src="/prisma-2.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                  xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
 
@@ -203,65 +221,82 @@ const Skills = () => {
                 <img
                   src="/stripe-4.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                  xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
-              <li>
-                <img
-                  src="/react.svg"
-                  className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
-                />
-              </li>
+
               <li>
                 <img
                   src="/nodejs.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                   xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
             </ul>
           </div>
           <div
             className="flex w-[1130px] h-[100px] relative bg-slate-100  text-gray-700 rounded-[10px] hover:border-2
-           hover:transition-all hover:duration-1000 hover:border-sky-700 hover:shadow-lg hover:pb-2"
+           hover:transition-all hover:duration-1000 hover:border-sky-700 hover:shadow-lg hover:pb-2
+            xs:flex xs:flex-col xs:w-[300px] xs:relative xs:items-center xs:h-[350px] xs:left-[290px] xs:bg-slate-950 xs:text-sky-300"
           >
-            <ul className="flex relative left-[155px]  space-x-2  hover:transition-all hover:duration-1000">
+            <ul
+              className="flex relative left-[155px]  space-x-2  hover:transition-all hover:duration-1000
+            xs:relative xs:flex xs:flex-col xs:top-[70px] xs:left-[60px] xs:items-center xs:justify-center gap-1"
+            >
               <li>
                 <img
                   src="/postgresql.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                   xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
               <li>
                 <img
                   src="/mysql-3.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                   xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
               <li>
                 <img
                   src="/mongodb.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                   xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
               <li>
                 <img
                   src="/firebase-1.svg"
                   className="w-[75px] h-[75px] mt-[10px] rounded-[10px] 
-                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700"
+                  hover:w-[80px] hover:h-[80px] hover:mt-[8px] hover:transition hover:duration-1000 hover:border-2 hover:border-sky-700
+                   xs:w-[45px] xs:h-[35px] xs:hover:w-[45px] xs:hover:h-[35px] xs:hover:mt-0 xs:hover:border-none"
                 />
               </li>
             </ul>
-            <h1 className="flex relative left-[370px] hover:text-sky-700 ">
-              <span className="text-[20px] leading-[20px] font-bold font-os flex relative left-[80px] top-[20px] hover:font-extrabold">
+            <h1
+              className="flex relative left-[370px] hover:text-sky-700  
+            xs:flex xs:flex-col xs:items-start xs:left-0 xs:relative xs:hover:text-sky-500 xs:top-[30px]"
+            >
+              <span
+                className="text-[20px] leading-[20px] font-bold font-os flex relative left-[80px] top-[20px] hover:font-extrabold
+               xs:relative xs:left-[30px] xs:flex xs:h-[50px] xs:w-[200px] xs:text-[25px] xs:font-mono xs:top-[-200px]"
+              >
                 Databases
               </span>
-              <span className="text-[15px] font-roboto leading-[15px] font-[400] flex relative top-[60px] left-[-82px] hover:font-medium">
+              <span className=" xs:hidden text-[15px] font-roboto leading-[15px] font-[400] flex relative top-[60px] left-[-82px] hover:font-medium">
                 PostgreSQL, MySQL, Mongodb, Firebase
+              </span>
+              <span
+                className="hidden text-[15px] font-roboto leading-[15px] h-[100px] w-[250px] font-[400]  relative top-[60px] left-[-45px] hover:font-[500]
+              xs:text-[20px] xs:flex xs:flex-col xs:leading-[50px] xs:relative xs:w-[80px] xs:h-[400px] xs:right-10 xs:top-[-200px] xs:ml-4 xs:text-center
+              "
+              >
+                PostgreSQL MySQL Mongodb Firebase
               </span>
             </h1>
           </div>
